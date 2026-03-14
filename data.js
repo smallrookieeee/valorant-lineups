9bac2bb4ba081b004a80fef326ccf14a71240c10
xsh <a2240930214@outlook.com>
2026-03-14 22:33:41 +0800

    添加了对蓄力反弹次数的描述

--- data.js
+++ data.js
@@ -11,26 +11,34 @@
 const data = {
     maps: [
         {
-            name: "隐世修所", // 地图名
+            // 地图对象：隐世修所
+            name: "隐世修所",
             heroes: [
                 {
+                    // 英雄对象：猎枭
                     name: "猎枭 (Sova)",
                     skills: [
                         {
-                            skill: "A包点深处探测箭",
-                            type: "探测",
-                            side: "进攻",
-                            pos: "A外台阶角落",
-                            aim: "对准树枝拐角处",
-                            effect: "探测A大",
-                            img_pos: "images/haven_sova_a_1_pos.png",
-                            img_aim: "images/haven_sova_a_1_aim.png",
-                            img_effect: "images/haven_sova_a_1_eff.png"
+                            // 技能点位对象 1
+                            skill: "A包点深处探测箭", // 技能名称
+                            type: "探测",                // 技能分类，例如探测、秒杀箭等
+                            side: "进攻",               // 阵营：进攻 / 防守
+                            power: "2格蓄力", 
+                            bounce: "1次弹跳",
+                            pos: "A外台阶角落",         // 站位描述
+                            aim: "对准树枝拐角处",       // 瞄准参考
+                            effect: "探测A大",          // 技能效果说明
+                            img_pos: "images/haven_sova_a_1_pos.png",    // 站位图
+                            img_aim: "images/haven_sova_a_1_aim.png",    // 瞄准图
+                            img_effect: "images/haven_sova_a_1_eff.png"  // 效果图
                         },
                         {
+                            // 技能点位对象 2
                             skill: "C包点防拆秒杀箭",
                             type: "秒杀箭",
                             side: "进攻",
+                            power: "2格蓄力", 
+                            bounce: "1次弹跳",
                             pos: "C厅入口大门处",
                             aim: "对准左侧山坡树尖",
                             effect: "防拆秒杀",
@@ -43,6 +51,7 @@ const data = {
             ]
         },
         {
+            // 地图对象：幽邃地窟
             name: "幽邃地窟",
             heroes: [
                 {
@@ -52,6 +61,8 @@ const data = {
                             skill: "A包点探测箭",
                             type: "探测",
                             side: "进攻",
+                            power: "满格蓄力", 
+                            bounce: "1次反弹",
                             pos: "A外箱子角落",
                             aim: "对准黄线与柱子交界处的右边",
                             effect: "A包全探测",
